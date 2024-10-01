e84f1bc862f71e5f5ae9d02849d41782a1ef42b3
chaitanya <g.chaitanya1419@gmail.com>
2024-10-01 08:00:03 +0530

    hey

---
+++ Version1/src/Components/savedPosts/index.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import axios  from "axios";
+
+const SavedPosts =  () => {
+
+    return(
+        <div>
+            <h1>Saved Posts</h1>    
+        </div>
+    )
+
+}
+
+export default SavedPosts
